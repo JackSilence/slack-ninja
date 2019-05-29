4aa245602fc93712452065abba25622fe80523e4
JackSilence <jacksilence@gmail.com>
2019-05-29 17:19:25 +0800

    init

---
+++ src/main/java/mrt/App.java
@@ -0,0 +1,16 @@
+package mrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import magic.service.AsyncExecutor;
+import magic.service.Slack;
+
+@SpringBootApplication
+@Import( { AsyncExecutor.class, Slack.class } )
+public class App {
+	public static void main( String[] args ) {
+		SpringApplication.run( App.class, args );
+	}
+}
---
+++ src/main/java/mrt/controller/QueryController.java
@@ -0,0 +1,39 @@
+package mrt.controller;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import magic.service.Slack;
+
+public class QueryController {
+	private final Logger log = LoggerFactory.getLogger( this.getClass() );
+
+	@Autowired
+	private Slack slack;
+
+	@PostMapping( "/query" )
+	public Map<String, String> execute( @RequestParam String text ) {
+		log.info( "Text: {}", text );
+
+		try {
+			String[] message = StringUtils.split( text );
+
+			Assert.isTrue( message.length == 2, "起訖站皆須輸入" );
+
+			return slack.text( "成功" );
+
+		} catch ( RuntimeException e ) {
+			log.error( "", e );
+
+			return slack.text( e.getMessage() );
+
+		}
+	}
+}
