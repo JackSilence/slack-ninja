de5b034980cd0cfa9b3947d7f09a8fc6e017ac4e
JackSilence <jacksilence@gmail.com>
2019-06-03 22:40:57 +0800

    調整文字內容

--- src/main/java/ninja/controller/TaskController.java
+++ src/main/java/ninja/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
+import magic.service.Slack;
 import net.gpedro.integrations.slack.SlackActionType;
 import net.gpedro.integrations.slack.SlackAttachment;
 import net.gpedro.integrations.slack.SlackMessage;
@@ -29,6 +31,9 @@ public class TaskController {
 
 	private static final Gson GSON = new Gson();
 
+	@Autowired
+	private Slack slack;
+
 	private enum Task {
 		SHOPEE( "百米家新商品通知" ), HEROKU( "Heroku Usage" ), POINT( "點數查詢" ), NBA( "NBA BOX" );
 
@@ -41,22 +46,22 @@ public class TaskController {
 
 	@PostMapping
 	public Map<?, ?> task() {
-		SlackAttachment attach = new SlackAttachment( StringUtils.EMPTY ).setTitle( "任務清單" ).setCallbackId( ID );
+		SlackAttachment attach = new SlackAttachment( StringUtils.EMPTY ).setCallbackId( ID ).setColor( "#3AA3E3" );
 
 		Stream.of( Task.values() ).map( this::action ).forEach( i -> attach.addAction( i ) );
 
-		return map( new SlackMessage( StringUtils.EMPTY ).addAttachments( attach.setText( "點擊按鈕以選擇，並於確認後執行" ) ).prepare() );
+		return map( new SlackMessage( StringUtils.EMPTY ).addAttachments( attach ).prepare() );
 	}
 
 	@PostMapping( "/execute" )
 	public Map<?, ?> execute( String payload ) {
 		log.info( payload );
 
-		return null;
+		return slack.text( "OK" );
 	}
 
 	private Action action( Task task ) { // "confirm": {} -> 會出現預設的確認視窗
-		return new Action( ID, task.desc, SlackActionType.BUTTON, task.name() ).setConfirm( new Confirm( "確認執行任務", task.desc, "是", "否" ) );
+		return new Action( ID, task.desc, SlackActionType.BUTTON, task.name() ).setConfirm( new Confirm() );
 	}
 
 	private Map<String, Object> map( JsonObject object ) {
