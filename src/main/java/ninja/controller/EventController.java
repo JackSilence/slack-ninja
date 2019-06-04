d8f0242cc36cd4872d1a3b96fbf89681598919c5
JackSilence <jacksilence@gmail.com>
2019-06-04 17:26:52 +0800

    Event api不該直接回傳內容

--- src/main/java/ninja/controller/EventController.java
+++ src/main/java/ninja/controller/EventController.java
@@ -1,50 +1,32 @@
 package ninja.controller;
 
-import org.apache.commons.lang3.EnumUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+import magic.service.Slack;
+import net.gpedro.integrations.slack.SlackMessage;
 import ninja.slack.Callback;
-import ninja.slack.Event;
 import ninja.util.Gson;
 
 @RestController
 public class EventController extends BaseController {
-	private static final String LIST_TASKS = "list tasks";
-
-	private enum Type {
-		APP_MENTION;
-	}
-
 	@Autowired
-	private TaskController controller;
+	private Slack slack;
 
 	@PostMapping( "/event" )
 	public Object event( @RequestAttribute( REQ_BODY ) String body ) {
 		Callback callback = Gson.from( body, Callback.class );
 
-		Event event = callback.getEvent();
-
-		String challenge = callback.getChallenge(), text = event.getText();
-
-		if ( challenge != null ) {
-			return challenge;
+		if ( callback.getChallenge() != null ) {
+			return callback;
 
 		}
 
-		log.info( "Body: {}", body );
-		Type type = EnumUtils.getEnumIgnoreCase( Type.class, event.getType() );
-		log.info( text );
-		log.info( type.toString() );
-		if ( Type.APP_MENTION.equals( type ) && StringUtils.containsIgnoreCase( text, LIST_TASKS ) ) {
-			return controller.task();
+		// 正常應該要用chat.postMessage
+		slack.call( new SlackMessage( "Event type: " + callback.getEvent().getType() ) );
 
-		} else {
-			return "OK";
-
-		}
+		return null;
 	}
 }
