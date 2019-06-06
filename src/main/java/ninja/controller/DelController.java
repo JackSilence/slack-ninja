f37179dd3967bd0d96e5102975e2e35f36d898a1
JackSilence <jacksilence@gmail.com>
2019-06-06 13:43:14 +0800

    add produces = "application/json"

--- src/main/java/ninja/controller/DelController.java
+++ src/main/java/ninja/controller/DelController.java
@@ -11,7 +11,7 @@ import magic.util.Utils;
 public class DelController {
 	private static final String TEMPLATE = "/template/datepicker.json";
 
-	@PostMapping( "/delete" )
+	@PostMapping( value = "/delete", produces = "application/json" )
 	public String delete() {
 		return String.format( Utils.getResourceAsString( TEMPLATE ), LocalDate.now() );
 	}
