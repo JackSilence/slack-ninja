a47d33b2b0be8a4a0756840872adcdaf2b03ced2
JackSilence <jacksilence@gmail.com>
2019-06-28 17:48:42 +0800

    EstimateTime可能會拿不到, 修正因此可能會造成的錯誤

--- src/main/java/ninja/controller/BusController.java
+++ src/main/java/ninja/controller/BusController.java
@@ -59,12 +59,16 @@ public class BusController extends BaseController {
 				return bus.stop( i ).contains( keyword ) && Arrays.asList( 0d, 1d ).contains( direction( i ) ); // 0: 去程, 1: 返程
 
 			} ).collect( Collectors.groupingBy( i -> bus.stop( i ), Collectors.toList() ) ).forEach( ( k, v ) -> {
-				log.info( k );
-				log.info( v.toString() );
 				message.addAttachments( Slack.attachment().setText( ":busstop:" + k ).setColor( "good" ).setFields( v.stream().map( i -> {
-					int time = ( ( Double ) i.get( "EstimateTime" ) ).intValue(), minutes = time / 60, seconds = time % 60;
+					Double time = ( Double ) i.get( "EstimateTime" );
 
-					String value = ( minutes > 0 ? minutes + "分" : StringUtils.EMPTY ) + seconds + "秒";
+					String value = StringUtils.EMPTY;
+
+					if ( time != null ) {
+						int seconds = time.intValue(), minutes = seconds / 60;
+
+						value = ( minutes > 0 ? minutes + "分" : StringUtils.EMPTY ) + seconds % 60 + "秒";
+					}
 
 					return field( "往".concat( direction( i ).equals( 0d ) ? destination : departure ), value );
 
