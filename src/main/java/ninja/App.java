37737cb17446f7eb487f664bf0b10d61776d44ba
JackSilence <jacksilence@gmail.com>
2019-07-01 00:21:05 +0800

    新增天氣預報每六小時排程

--- src/main/java/ninja/App.java
+++ src/main/java/ninja/App.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import magic.controller.ExecuteController;
 import magic.service.AsyncExecutor;
 import magic.service.Slack;
 
 @SpringBootApplication
-@Import( { AsyncExecutor.class, Slack.class } )
+@Import( { ExecuteController.class, AsyncExecutor.class, Slack.class } )
 public class App {
 	public static void main( String[] args ) {
 		SpringApplication.run( App.class, args );
---
+++ src/main/java/ninja/task/Weather.java
@@ -0,0 +1,34 @@
+package ninja.task;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import magic.service.IService;
+import magic.util.Utils;
+import ninja.controller.WeatherController;
+
+@Service
+public class Weather implements IService {
+	private final Logger log = LoggerFactory.getLogger( this.getClass() );
+
+	@Autowired
+	private WeatherController weather;
+
+	@Value( "${slack.webhook.url:}" )
+	private String url;
+
+	@Scheduled( cron = "0 0 0,6,12,18 * * *" )
+	@Override
+	public void exec() {
+		Request request = Request.Post( url ).bodyString( weather.weather( "scheduled-task", StringUtils.EMPTY ), ContentType.APPLICATION_JSON );
+
+		log.info( Utils.getEntityAsString( request ) );
+	}
+}
