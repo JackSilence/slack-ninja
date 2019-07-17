3504850435483bc141641fe65c26f23671948e10
JackSilence <jacksilence@gmail.com>
2019-07-17 15:29:31 +0800

    add AQI task

---
+++ src/main/java/ninja/task/AQI.java
@@ -0,0 +1,20 @@
+package ninja.task;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import ninja.controller.AQIController;
+
+@Service
+public class AQI extends Task {
+	@Autowired
+	private AQIController aqi;
+
+	@Scheduled( cron = "0 0 * * * *" )
+	@Override
+	public void exec() {
+		exec( aqi.aqi( COMMAND, StringUtils.EMPTY ) );
+	}
+}
