2da3c95e10dae1be4d855fe8cb8ac1b2adbd491b
JackSilence <jacksilence@gmail.com>
2019-07-18 17:29:09 +0800

    修正

--- src/main/java/ninja/controller/OptionController.java
+++ src/main/java/ninja/controller/OptionController.java
@@ -42,9 +42,8 @@ public class OptionController extends BaseController {
 			Filter county = Filter.COUNTY, site = Filter.SITE_NAME;
 
 			return options( AQI.call( county.eq( value ) ).stream().map( i -> {
-				log.info( option( String.join( StringUtils.SPACE, Cast.string( i, county.toString() ), Cast.string( i, site.toString() ) ) ).toString() );
-				return option( String.join( StringUtils.SPACE, Cast.string( i, county.toString() ), Cast.string( i, site.toString() ) ) );
-			} ) );
+				return String.join( StringUtils.SPACE, Cast.string( i, county.toString() ), Cast.string( i, site.toString() ) );
+			} ).map( i -> ImmutableMap.of( "text", i, "value", i ) ) );
 		}
 
 		if ( Dialog.BUS.name().equals( id ) ) {
