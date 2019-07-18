8f5abe353eee1e188195d49bd689e7e9fadc2491
JackSilence <jacksilence@gmail.com>
2019-07-18 21:42:33 +0800

    AQI log拿掉

--- src/main/java/ninja/util/AQI.java
+++ src/main/java/ninja/util/AQI.java
@@ -4,20 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.http.client.fluent.Request;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.net.UrlEscapers;
 
 import magic.util.Utils;
 
 public class AQI {
-	private static final Logger log = LoggerFactory.getLogger( Jsoup.class );
-
-	public static final String API_URL = "http://opendata.epa.gov.tw/ws/Data/AQI/?$format=json&$filter=";
+	private static final String API_URL = "http://opendata.epa.gov.tw/ws/Data/AQI/?$format=json&$filter=";
 
 	public static List<Map<String, ?>> call( String filter ) {
-		log.error( API_URL + UrlEscapers.urlFragmentEscaper().escape( filter ) );
 		return Gson.list( Utils.getEntityAsString( Request.Get( API_URL + UrlEscapers.urlFragmentEscaper().escape( filter ) ) ) );
 	}
 }
