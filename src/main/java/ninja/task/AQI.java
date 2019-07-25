37a8327e9d6a690ac39849ce34649e5e74e95c89
JackSilence <jacksilence@gmail.com>
2019-07-25 09:25:55 +0800

    因應昨日新增ControllerAdvice, @Retryable的value調整為RunTimeException

--- src/main/java/ninja/task/AQI.java
+++ src/main/java/ninja/task/AQI.java
@@ -16,7 +16,7 @@ public class AQI extends Task {
 	private AQIController aqi;
 
 	@Scheduled( cron = "0 30 7-23 * * *", zone = Zone.TAIPEI )
-	@Retryable( value = IllegalStateException.class, backoff = @Backoff( 30000 ) )
+	@Retryable( value = RuntimeException.class, backoff = @Backoff( 30000 ) )
 	@Override
 	public void exec() {
 		exec( aqi.aqi( COMMAND, StringUtils.EMPTY ) );
