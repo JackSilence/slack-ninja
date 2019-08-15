f0979781ab9f98194952a21564d03c5f7aec4278
JackSilence <jacksilence@gmail.com>
2019-08-15 14:46:11 +0800

    調整

--- src/main/java/ninja/ex/ErrorAttrs.java
+++ src/main/java/ninja/ex/ErrorAttrs.java
@@ -2,6 +2,7 @@ package ninja.ex;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
@@ -10,6 +11,11 @@ import org.springframework.web.context.request.RequestAttributes;
 public class ErrorAttrs extends DefaultErrorAttributes {
 	@Override
 	public Map<String, Object> getErrorAttributes( RequestAttributes requestAttributes, boolean includeStackTrace ) {
-		return null;
+		Map<String, Object> attrs = super.getErrorAttributes( requestAttributes, includeStackTrace );
+
+		attrs.put( "exception", StringUtils.EMPTY );
+		attrs.put( "message", StringUtils.EMPTY );
+
+		return attrs;
 	}
 }
