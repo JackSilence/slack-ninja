9bff27656f06d864da3588eb8de1623cc916646f
JackSilence <jacksilence@gmail.com>
2020-08-13 17:32:10 +0800

    bug fix

--- src/main/java/ninja/task/TestTask.java
+++ src/main/java/ninja/task/TestTask.java
@@ -3,6 +3,8 @@ package ninja.task;
 import java.util.Arrays;
 import java.util.Date;
 
+import com.google.common.net.UrlEscapers;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -24,12 +26,12 @@ public class TestTask implements IService {
     @Scheduled( cron = "0 0 18 * * *", zone = Zone.TAIPEI )
     @Override
     public void exec() {
-        String thsr = String.format( "/thsr 南港 台中 %tF 10:00 出發", new Date() );
+        String thsr = String.format( "thsr 南港 台中 %tF 10:00 出發", new Date() );
 
-        Arrays.asList( "/bus 內湖幹線 \"康寧醫院\"", "/movie 哈拉影城", "/mrt 中山 松山", "/station 康寧醫院 捷運內湖站", "/theater 哈拉影城", thsr ).forEach( i -> {
+        Arrays.asList( "bus 內湖幹線 \"康寧醫院\"", "movie 哈拉影城", "mrt 中山 松山", "station 康寧醫院 捷運內湖站", "theater 哈拉影城", thsr ).forEach( i -> {
             String[] params = i.split( " ", 2 );
 
-            Utils.call( String.format( COMMAND_URL, token, channel, params[ 0 ], params[ 1 ] ) );
+            Utils.call( String.format( COMMAND_URL, token, channel, params[ 0 ], UrlEscapers.urlFragmentEscaper().escape( params[ 1 ] ) ) );
         } );
     }
 }
