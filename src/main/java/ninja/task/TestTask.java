98a3ed338a813c2777da4597681d6f3c0f0a2a09
JackSilence <jacksilence@gmail.com>
2021-02-17 15:19:45 +0800

    隨機使用legacy token

--- src/main/java/ninja/task/TestTask.java
+++ src/main/java/ninja/task/TestTask.java
@@ -1,10 +1,15 @@
 package ninja.task;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.common.net.UrlEscapers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,10 +20,9 @@ import ninja.util.Utils;
 
 @Service
 public class TestTask implements IService {
-    private static final String COMMAND_URL = "https://slack.com/api/chat.command?token=%s&channel=%s&command=/%s&text=%s";
+    private final Logger log = LoggerFactory.getLogger( this.getClass() );
 
-    @Value( "${slack.legacy.token:}" )
-    private String token;
+    private static final String COMMAND_URL = "https://slack.com/api/chat.command?token=%s&channel=%s&command=/%s&text=%s";
 
     @Value( "${slack.channel:}" )
     private String channel;
@@ -26,12 +30,16 @@ public class TestTask implements IService {
     @Scheduled( cron = "0 0 18 * * *", zone = Zone.TAIPEI )
     @Override
     public void exec() {
-        String thsr = String.format( "thsr 南港 台中 %tF 10:00 出發", new Date() );
+        List<String> tokens = System.getenv().keySet().stream().filter( i -> i.startsWith( "slack.legacy.token" ) ).collect( Collectors.toList() );
+
+        String thsr = String.format( "thsr 南港 台中 %tF 10:00 出發", new Date() ), token;
+
+        log.info( "Token: {}", token = tokens.get( Calendar.getInstance().get( Calendar.DAY_OF_WEEK ) % tokens.size() ) );
 
         Arrays.asList( "bus 內湖幹線 \"康寧醫院\"", "movie 哈拉影城", "mrt 中山 松山", "station 康寧醫院 捷運內湖站", "theater 哈拉影城", "dict pig", thsr ).forEach( i -> {
             String[] params = i.split( " ", 2 );
 
-            Utils.call( String.format( COMMAND_URL, token, channel, params[ 0 ], UrlEscapers.urlFragmentEscaper().escape( params[ 1 ] ) ) );
+            Utils.call( String.format( COMMAND_URL, System.getenv( token ), channel, params[ 0 ], UrlEscapers.urlFragmentEscaper().escape( params[ 1 ] ) ) );
         } );
     }
 }
