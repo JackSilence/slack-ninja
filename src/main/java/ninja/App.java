7d728dab9baa168278a9bd0549cdced17f092aea
JackSilence <jacksilence@gmail.com>
2022-09-08 12:31:15 +0800

    Add BuyMiJiaTask

--- src/main/java/ninja/App.java
+++ src/main/java/ninja/App.java
@@ -1,12 +1,16 @@
 package ninja;
 
+import java.util.Properties;
 import java.util.concurrent.Executor;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -14,14 +18,42 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import magic.controller.ExecuteController;
 import magic.service.AsyncExecutor;
 import magic.service.Slack;
+import magic.util.Utils;
 import ninja.ex.EXHandler;
 
 @SpringBootApplication
 @EnableRetry
 @Import( { ExecuteController.class, AsyncExecutor.class, Slack.class } )
 public class App implements AsyncConfigurer {
+	@Value( "${email.username:}" )
+	private String username;
+
+	@Value( "${email.password:}" )
+	private String password;
+
 	public static void main( String[] args ) {
 		SpringApplication.run( App.class, args );
+	}
+
+	@Bean
+	public JavaMailSender sender() {
+		JavaMailSenderImpl sender = new JavaMailSenderImpl();
+
+		sender.setHost( "smtp.gmail.com" );
+		sender.setPort( 465 );
+
+		sender.setUsername( Utils.decode( username ) );
+		sender.setPassword( Utils.decode( password ) );
+
+		Properties props = sender.getJavaMailProperties();
+
+		props.put( "mail.transport.protocol", "smtp" );
+		props.put( "mail.smtp.auth", "true" );
+		props.put( "mail.smtp.starttls.enable", "true" );
+		props.put( "mail.debug", "false" );
+		props.put( "mail.smtp.ssl.enable", "true" );
+
+		return sender;
 	}
 
 	@Bean
---
+++ src/main/java/ninja/service/Mail.java
@@ -0,0 +1,44 @@
+package ninja.service;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import magic.service.SendGrid;
+
+@Service
+public class Mail extends SendGrid {
+	@Autowired
+	private JavaMailSender sender;
+
+	@Value( "${gmail.mail.to:}" )
+	private String mail;
+
+	@Override
+	public void send( String subject, String content ) {
+		super.send( subject, content );
+
+		try {
+			MimeMessage message = sender.createMimeMessage();
+
+			MimeMessageHelper helper = new MimeMessageHelper( message, true, StandardCharsets.UTF_8.name() );
+
+			helper.setSubject( subject );
+			helper.setText( content, true );
+			helper.setTo( mail );
+
+			sender.send( message );
+
+		} catch ( MessagingException e ) {
+			throw new RuntimeException( "Failed to send: " + subject, e );
+
+		}
+	}
+}
