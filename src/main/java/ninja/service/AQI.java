8a78836ffd3a3a781c66f5b5c905f8790fc285ed
Jack <35412362+JackSilence@users.noreply.github.com>
2023-10-23 13:46:22 +0800

    Update AQI.java

--- src/main/java/ninja/service/AQI.java
+++ src/main/java/ninja/service/AQI.java
@@ -3,6 +3,8 @@ package ninja.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
 import org.springframework.stereotype.Service;
 
 import ninja.util.Gson;
@@ -10,22 +12,24 @@ import ninja.util.Utils;
 
 @Service
 public class AQI extends Data<List<String>> {
-	private static final String SITE_URL = "https://airtw.epa.gov.tw/ajax.aspx?Target=Get%s&%s=";
+	private static final String SITE_URL = "https://airtw.moenv.gov.tw/ajax.aspx";
+
+	private static final String SITE_PAYLOAD = "Target=Get%s&%s";
 
 	private static final String COUNTY = "County", NAME = "Name";
 
 	@Override
 	void init( Map<String, List<String>> data ) {
-		call( url( COUNTY, "AreaID=&SiteID" ) ).forEach( i -> {
-			data.put( i.get( NAME ), Utils.list( call( url( "Site", COUNTY ) + i.get( "Value" ) ).stream().map( j -> j.get( NAME ) ) ) );
+		call( request( COUNTY, "AreaID=&SiteID=" ) ).forEach( i -> {
+			data.put( i.get( NAME ), Utils.list( call( request( "Site", COUNTY + "=" + i.get( "Value" ) ) ).stream().map( j -> j.get( NAME ) ) ) );
 		} );
 	}
 
-	private List<Map<String, String>> call( String uri ) {
-		return Gson.list( Utils.call( uri ) );
+	private List<Map<String, String>> call( Request request ) {
+		return Gson.list( Utils.call( request ) );
 	}
 
-	private String url( String target, String field ) {
-		return String.format( SITE_URL, target, field );
+	private Request request( String target, String query ) {
+		return Request.Post( SITE_URL ).bodyString( String.format( SITE_PAYLOAD, target, query ), ContentType.APPLICATION_FORM_URLENCODED );
 	}
 }
