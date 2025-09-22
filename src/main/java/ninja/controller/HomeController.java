404262af7bf32d95cc7b132a0cf0f85a5288add7
JackSilence <jacksilence@gmail.com>
2025-09-22 12:59:32 +0800

    add HomeController

---
+++ src/main/java/ninja/controller/HomeController.java
@@ -0,0 +1,12 @@
+package ninja.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping( "/" )
+    public String home() {
+        return "Slack Ninja Bot is running! 🥷";
+    }
+}
